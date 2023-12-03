7031b7195714aee6d2ea988584c831d2b362750c
Humoyun <h.ahmedov209@gmail.com>
2023-12-03 18:58:25 +0500

    🌐 Production

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -9,14 +9,18 @@ ADMINS = ('humoyun209', 'h.ahmedov209@gmail.com')
 ALLOWED_HOSTS = ['*']
 
 
-DATABASE = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASS'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': 'db',
         'PORT': 5432
     }
 }
 
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
